4aaf5fb023526b5242c05fdb86a1b7a759a0e808
Watson Dinh <ping@w47s0n.com>
2025-11-08 19:06:37 -0600

    get away from neotree

--- dotfiles/nvim/lua/config/autocmds.lua
+++ dotfiles/nvim/lua/config/autocmds.lua
@@ -12,14 +12,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
-vim.api.nvim_create_autocmd({ "BufLeave" }, {
-  pattern = { "*lazygit*" },
-  group = vim.api.nvim_create_augroup("git_refresh_neotree", { clear = true }),
-  callback = function()
-    require("neo-tree.sources.filesystem.commands").refresh(require("neo-tree.sources.manager").get_state("filesystem"))
-  end,
-})
-
 -- Open build.sbt if it exists in the current directory on startup
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
   callback = function()
