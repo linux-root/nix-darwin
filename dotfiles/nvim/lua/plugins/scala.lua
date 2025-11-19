43d80cffed512d1f4b9d04b32f535ef0aa0575eb
Watson Dinh <ping@w47s0n.com>
2025-11-19 06:58:57 -0600

    scala

--- dotfiles/nvim/lua/plugins/scala.lua
+++ dotfiles/nvim/lua/plugins/scala.lua
@@ -5,89 +5,93 @@ return {
   },
   {
     "scalameta/nvim-metals",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-  keys = {
-    {
-      "<leader>me",
-      function()
-        require("telescope").extensions.metals.commands()
-      end,
-      desc = "Metals commands",
+    keys = {
+      {
+        "<localleader>e",
+        function()
+          require("metals").commands()
+        end,
+        ft = { "scala", "sbt", "java" },
+        desc = "Metals commands",
+      },
+      {
+        "<localleader>c",
+        function()
+          require("metals").compile_cascade()
+        end,
+        desc = "Metals compile cascade",
+      },
+      {
+        "<localleader>h",
+        function()
+          require("metals").hover_worksheet()
+        end,
+        desc = "Metals hover worksheet",
+      },
     },
-    {
-      "<leader>mc",
-      function()
-        require("metals").compile_cascade()
-      end,
-      desc = "Metals compile cascade",
-    },
-    {
-      "<leader>mh",
-      function()
-        require("metals").hover_worksheet()
-      end,
-      desc = "Metals hover worksheet",
-    },
+    ft = { "scala", "sbt", "java" },
+    opts = function()
+      local metals_config = require("metals").bare_config()
+
+      metals_config.init_options.statusBarProvider = "off"
+
+      metals_config.settings = {
+        verboseCompilation = true,
+        showImplicitArguments = false,
+        showImplicitConversionsAndClasses = false,
+        showInferredType = true,
+        superMethodLensesEnabled = true,
+        excludedPackages = {
+          "akka.actor.typed.javadsl",
+          "org.apache.pekko.actor.typed.javadsl",
+          "com.github.swagger.akka.javadsl",
+        },
+        testUserInterface = "Test Explorer",
+      }
+
+      metals_config.on_attach = function(client, bufnr)
+        -- your on_attach function
+        require("metals").setup_dap()
+      end
+
+      return metals_config
+    end,
+    config = function(self, metals_config)
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = self.ft,
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end,
   },
-  ft = { "scala", "sbt", "sc" },
-  opts = function()
-    local metals_config = require("metals").bare_config()
-    local dap = require("dap")
-    dap.listeners.after["event_terminated"]["nvim-metals"] = function()
-      dap.repl.open()
-    end
-    dap.configurations.scala = {
-      {
-        type = "scala",
-        request = "launch",
-        name = "RunOrTest",
-        metals = {
-          runType = "runOrTestFile",
-          --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      -- Debug settings
+      local dap = require("dap")
+      dap.configurations.scala = {
+        {
+          type = "scala",
+          request = "launch",
+          name = "RunOrTest",
+          metals = {
+            runType = "runOrTestFile",
+            --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+          },
         },
-      },
-      {
-        type = "scala",
-        request = "launch",
-        name = "Test Target",
-        metals = {
-          runType = "testTarget",
+        {
+          type = "scala",
+          request = "launch",
+          name = "Test Target",
+          metals = {
+            runType = "testTarget",
+          },
         },
-      },
-    }
-
-    metals_config.init_options.statusBarProvider = "off"
-
-    metals_config.settings = {
-      verboseCompilation = true,
-      showImplicitArguments = true,
-      showImplicitConversionsAndClasses = true,
-      showInferredType = true,
-      superMethodLensesEnabled = true,
-      excludedPackages = {
-        "akka.actor.typed.javadsl",
-        "org.apache.pekko.actor.typed.javadsl",
-        "com.github.swagger.akka.javadsl",
-      },
-      testUserInterface = "Test Explorer",
-    }
-
-    metals_config.on_attach = function(client, bufnr)
-      -- your on_attach function
-      require("metals").setup_dap()
-    end
-
-    return metals_config
-  end,
-  config = function(self, metals_config)
-    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = self.ft,
-      callback = function()
-        require("metals").initialize_or_attach(metals_config)
-      end,
-      group = nvim_metals_group,
-    })
-  end,
+      }
+    end,
   },
 }
