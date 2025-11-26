7159fe0f4ae3241c9f5c6ecc0ea8e9d97927becd
Watson Dinh <ping@w47s0n.com>
2025-11-26 07:57:44 -0600

    fix home-manager version mismatch by explicitly using master branch

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
