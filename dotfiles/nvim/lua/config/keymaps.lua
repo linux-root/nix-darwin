4189819d5dc2927ee12dfab207a263cc68351185
Watson Dinh <ping@w47s0n.com>
2026-02-02 18:08:19 -0600

    coda

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -2,3 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- Resize window using <ctrl> arrow keys
+
+-- Exit terminal insert mode with <Esc>
+-- This can be problematic if you:
+-- - Run vim/nvim inside the terminal (you won't be able to exit insert mode in the nested vim)
+-- - Use TUI applications that need <Esc> (like htop, lazygit, etc.)
+-- - Use shell vi mode where <Esc> switches to normal mode
+-- - Run any CLI program that uses <Esc> for navigation or closing dialogs
+
+-- Exit terminal mode with Escape
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
